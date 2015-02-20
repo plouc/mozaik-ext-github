9d35bac326ed7cea6c394a3b7071c70d785b2741
Raphaël Benitte <benitteraphael@gmail.com>
2015-02-20 09:37:04 +0100

    Add npm pre publish script to package.json

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,7 @@
-var components = {
+module.exports = {
     Branches:                    require('./Branches.jsx'),
     PullRequests:                require('./PullRequests.jsx'),
     IssueLabelsTreemap:          require('./IssueLabelsTreemap.jsx'),
     UserBadge:                   require('./UserBadge.jsx'),
     RepositoryContributorsStats: require('./RepositoryContributorsStats.jsx')
 };
-
-require('mozaik/browser')
-    .add('github.branches',                      components.Branches)
-    .add('github.pull_requests',                 components.PullRequests)
-    .add('github.issue_labels_treemap',          components.IssueLabelsTreemap)
-    .add('github.user_badge',                    components.PullRequests)
-    .add('github.repository_contributors_stats', components.RepositoryContributorsStats)
-;
-
-module.exports = components;
