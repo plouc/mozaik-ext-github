67d5166217fff7b56429514322f7dde781f140fe
Raphaël Benitte <benitteraphael@gmail.com>
2016-03-18 11:39:48 +0900

    chore(demo): remove demo files as there's now a dedicated branch for this

--- demo/app.js
+++
@@ -1,9 +0,0 @@
-require('babel/register')({
-    only: /node_modules\/mozaik[^/]*\/src/
-});
-
-var mozaik = new (require('mozaik'))(require('./config'));
-
-mozaik.bus.registerApi('github', require('mozaik-ext-github/client'));
-
-mozaik.startServer();
